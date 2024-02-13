f1bc1b091990b9f04796eb5e317effb4b9992243
Jeremy Yang <jeremyjyang@gmail.com>
2024-02-13 15:37:39 -0700

    Ok.

--- R/biomarkers_loinc_hf_pt.R
+++ R/biomarkers_loinc_hf_pt.R
@@ -11,10 +11,18 @@ hf_pt <- read_delim("cerner_hf_data/hf_labs-selected_pt_OUT.tsv", delim = "\t")
 setDT(hf_pt)
 head(hf_pt)
 
-sprintf("Columns: %s", paste(names(hf_pt), collapse=", "))
+message(sprintf("Columns: %s", paste(names(hf_pt), collapse=", ")))
+message(sprintf("Encounters: %s", hf_pt[, uniqueN(encounter_id)]))
+message(sprintf("Lab_procedures: %s", hf_pt[, uniqueN(lab_procedure_id)]))
 
-sprintf("Encounters: %s", hf_pt[, uniqueN(encounter_id)])
-sprintf("Lab_procedures: %s", hf_pt[, uniqueN(lab_procedure_id)])
+hf_pt[, admitted_year := year(admitted_dt_tm)]
+table(hf_pt[, admitted_year])
+
+y <- 2016
+message(sprintf("Sample one year for computability: %d", y))
+
+hf_pt <- hf_pt[admitted_year == y]
+
 
 table(hf_pt[, gender])
 
