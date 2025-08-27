c5be13e9f092597c37832ea8c7a64f03f0b0a8aa
Jeremy Yang <jeremyjyang@gmail.com>
2025-08-27 15:18:37 -0600

    Ok.

--- sql/hf_labs-selected_results_agg-Troponin_FULL-2018_pt_exclusion.sql
+++ sql/hf_labs-selected_results_agg-Troponin_FULL-2018_pt_exclusion.sql
@@ -1,16 +1,23 @@
 -- For selected LOINC codes, analysis of empirical distributions.
-SELECT DISTINCT
-	jpd.patient_id
+ALTER TABLE jjyang.hf_f_lab_troponin_2018_patients
+        ADD COLUMN IF NOT EXISTS exclusion_flag BOOLEAN DEFAULT FALSE ;
+UPDATE jjyang.hf_f_lab_troponin_2018_patients jp
+        SET exclusion_flag = TRUE
+        WHERE jp.patient_id IN (
+               SELECT
+                        jpd.patient_id
+                FROM
+                        jjyang.hf_f_lab_troponin_2018_pt_dx jpd
+                WHERE
+                        jpd.diagnosis_description ILIKE '%heart%'
+                        OR jpd.diagnosis_description ILIKE '%cardiac%'
+        )
+        ;
+SELECT
+        COUNT(jp.patient_id),
+        jp.exclusion_flag
 FROM
-	jjyang.hf_f_lab_troponin_2018_pt_dx jpd
-WHERE
-	jpd.patient_id NOT IN (
-		SELECT
-			jpd.patient_id
-		FROM
-			jjyang.hf_f_lab_troponin_2018_pt_dx jpd
-		WHERE
-			jpd.diagnosis_description ILIKE '%heart%'
-			OR jpd.diagnosis_description ILIKE '%cardiac%'
-	)
-	;
+        jjyang.hf_f_lab_troponin_2018_patients jp
+GROUP BY
+        jp.exclusion_flag
+        ;
