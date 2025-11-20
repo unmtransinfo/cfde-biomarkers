42312c34f80b97c11007501464acd3d8dca56aaa
Jeremy Yang <jeremyjyang@gmail.com>
2025-11-20 13:12:12 -0700

    Alright.

---
+++ sql/hf_labs-selected_results_agg-Creatinine_FULL-2018_pt_exclusion.sql
@@ -0,0 +1,23 @@
+-- For selected LOINC codes, analysis of empirical distributions.
+ALTER TABLE jjyang.hf_f_lab_creatinine_2018_patients
+        ADD COLUMN IF NOT EXISTS exclusion_flag BOOLEAN DEFAULT FALSE ;
+UPDATE jjyang.hf_f_lab_creatinine_2018_patients jp
+        SET exclusion_flag = TRUE
+        WHERE jp.patient_id IN (
+               SELECT
+                        jpd.patient_id
+                FROM
+                        jjyang.hf_f_lab_creatinine_2018_pt_dx jpd
+                WHERE
+                        jpd.diagnosis_description ILIKE '%kidney%disease%'
+                        OR jpd.diagnosis_description ILIKE '%kidney%failure%'
+        )
+        ;
+SELECT
+        COUNT(jp.patient_id),
+        jp.exclusion_flag
+FROM
+        jjyang.hf_f_lab_creatinine_2018_patients jp
+GROUP BY
+        jp.exclusion_flag
+        ;
