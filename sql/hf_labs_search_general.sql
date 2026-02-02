189a972e300992584ca2cc6b3bea019e24e54293
Jeremy Yang <jeremyjyang@gmail.com>
2026-02-02 12:56:40 -0700

    Alright.

---
+++ sql/hf_labs_search_general.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM hf_d_lab_procedure
+WHERE
+        (lab_procedure_name ILIKE '%Tryptophan%'
+        OR lab_procedure_name ILIKE '%Bilirubin%'
+        OR lab_procedure_name ILIKE '%Phosphatase%'
+        OR lab_procedure_name ILIKE '%Serum or Plasma%'
+        OR lab_procedure_name ILIKE '%Blood%'
+        OR lab_procedure_name ILIKE '%Urine%'
+        )
+        AND lab_procedure_group = 'General Test'
+        AND lab_super_group = 'General Test'
+        ;
